06fc45e15c57b5d2341f8cf39ac41d1e0269897f
Cody Collicott <cody@dashboard.ca>
2015-10-05 12:36:22 -0400

    5.0.0.0

---
+++ github-updater.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Clone
+ *
+ * @package   cloned
+ * @author    Andy Fragen
+ * @license   GPL-2.0+
+ * @link      https://github.com/dashboard-repo/cloned
+ */
+
+/*
+Plugin Name:       GitHub Updater
+Plugin URI:        https://github.com/dashboard-repo/cloned
+Description:       tester
+Version:           5.0.0.0
+Author:            cody
+License:           GNU General Public License v2
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+GitHub Plugin URI: https://github.com/dashboard-repo/cloned
+GitHub Branch:     master
+Requires WP:       3.8
+Requires PHP:      5.3
+*/
+
+/*
+ * Exit if called directly.
+ * PHP version check and exit.
+ */
