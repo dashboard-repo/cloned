ae879f408d82bc0b270c76285d6f6b98555a351b
Cody Collicott <cody@dashboard.ca>
2015-10-05 12:46:03 -0400

    2

--- github-updater.php
+++ github-updater.php
@@ -3,8 +3,8 @@
 Plugin Name:       GitHub Updater
 Plugin URI:        https://github.com/dashboard-repo/cloned
 Description:       A plugin to automatically update GitHub, Bitbucket or GitLab hosted plugins and themes. It also allows for remote installation of plugins or themes into WordPress.
-Version:           1.0.0
-Author:            Andy Fragen
+Version:           2.0.0
+Author:            Cody Collicott
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
 Domain Path:       /languages
